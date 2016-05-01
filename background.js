7bc1d470e5a165c5ae75ec8e36e9d3a71f1d1103
John Allen <johnm.allen@msn.com>
2016-05-01 15:23:44 -0700

    Segregate removed elements by domain

--- background.js
+++ background.js
@@ -1,5 +1,13 @@
-/* This will save all of the deleted element list. */
-var removedElements = [];
+/* This will save all of the removed element list. */
+// [domain][elements]
+var removedElements = Object();
+
+/* Get domain. */
+function url_domain(url) {
+  var a = document.createElement('a');
+  a.href = url;
+  return a.hostname;
+}
 
 /* Send request to remove element. */
 var removeElement = function(e) {
@@ -11,13 +19,26 @@ var removeElement = function(e) {
 /* Return Removed Elements. */
 function returnRemovedElements() {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-	  chrome.tabs.sendMessage(tabs[0].id, {text:removedElements});
+	  var url = tabs[0].url;
+	  var domain = url_domain(url);
+	  console.log(removedElements);
+	  chrome.tabs.sendMessage(tabs[0].id, {text:removedElements[domain]});
 	});
 };
 
 /* Store Removed Elements. */
 function storeRemovedElements(elementSelector){
-	removedElements.push(elementSelector);
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	  var url = tabs[0].url;
+	  var domain = url_domain(url);
+	  if (typeof removedElements[domain] === 'undefined') {
+	  	// url => [selector]
+	  	removedElements[domain] = [elementSelector];
+	  } else {
+	  	// url => [selector1, selector2]
+	  	removedElements[domain].push(elementSelector);
+	  }
+	});
 }
 
 /* Handle Requests. */
@@ -25,7 +46,6 @@ chrome.runtime.onMessage.addListener(function(message){
 	if(typeof message.elementRemoved === 'string') {
 		// Handle store element request.
 		storeRemovedElements(message.elementRemoved);
-		alert('Total Elements Removed: ' + removedElements.length);
 	} else if(message.getElements === true) {
 		// Handle get elements request.
 		returnRemovedElements();
